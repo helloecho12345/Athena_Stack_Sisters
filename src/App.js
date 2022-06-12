09d2055465f654e02c2051a74741cb28c1e76936
Kitty Mo Yin Ho <kmoyinho@gmail.com>
2022-06-12 11:28:27 +0100

    Last minute additions lol

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 //import logo from './logo.svg';
 import './App.css';
-//import Test from './components/Test';
 import Header from './components/Header';
 import EducationBody from './components/EducationBody';
 import WorkBody from './components/WorkBody';
@@ -9,6 +8,7 @@ import LegalBody from './components/LegalBody';
 import HealthBody from './components/HealthBody';
 import EmergencyBody from './components/EmergencyBody';
 import HomeBody from './components/HomeBody';
+import Footer from './components/Footer';
 
 function App() {
     return ( <
@@ -29,6 +29,7 @@ function App() {
         HealthBody / >
         <
         EmergencyBody / >
+        <Footer />
         <
         /div>
     );
---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+const Footer = () => (
+    <div class="footer bg-primary text-white text-center">
+        <p> &copy; 2022 FirstStep</p>
+    </div>
+)
+
+export default Footer
