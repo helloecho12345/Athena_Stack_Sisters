853add9eb5f96fea3a5191eb5cc820aa65da1e3d
Kitty Mo Yin Ho <kmoyinho@gmail.com>
2022-06-12 01:13:27 +0100

    Added css and html

---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+const Body = () => (
+    <body>
+        {/*<!-- <div id="services" class="container-fluid text-center"></div> -->*/}
+        <header class="masthead bg-primary text-white text-center" id="page-top">
+            <div class="container d-flex align-items-center flex-column">
+                {/* <!-- Masthead Heading --> */}
+                <h1 class="masthead-heading text-uppercase mb-0">Welcome to United Kingdom</h1>
+            </div>
+
+            <div class="d-grid gap-2 d-md-flex justify-content-md-center">
+
+                <button type="button" class="btn btn-primary btn-circle btn-xl"><i class="fa solid fa-briefcase"></i></button>
+                <button type="button" class="btn btn-primary btn-circle btn-xl"><i class="fa-solid fa-stethoscope"></i></button>
+                <button type="button" class="btn btn-primary btn-circle btn-xl"><i class="fa-solid fa-house"></i></button>
+                <button type="button" class="btn btn-primary btn-circle btn-xl"><i class="fa-solid fa-scale-unbalanced-flip"></i></button>
+                <button type="button" class="btn btn-primary btn-circle btn-xl"><i class="fa-solid fa-hand"></i></button>
+                <button type="button" class="btn btn-primary btn-circle btn-xl"><i class="fa-solid fa-graduation-cap"></i></button>
+                <button type="button" class="btn btn-primary btn-circle btn-xl"><i class="fa-solid fa-circle-exclamation"></i></button>
+            </div>
+        </header>
+    </body>
+)
+
+export default Body
---
+++ src/components/Test.js
@@ -0,0 +1,18 @@
+const Test = () => (
+    <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+            Hello world. TEST htmlContent.html.
+        </p>
+        <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+        >
+            Learn React
+        </a>
+    </header>
+)
+
+export default Test
